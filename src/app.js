071ddcc6c3553c28ee54296c958a510032258c80
Viktorwahlqvist <wahlqvistviktor@hotmail.com>
2024-12-04 13:40:07 +0100

    updating comments etc

--- src/app.js
+++ src/app.js
@@ -80,32 +80,31 @@ const fetchDataWithKey = async () => {
         if (data.bodies && searchBtn) {
             // Add an event listener to the search button with a click event
             searchBtn.addEventListener('click', (event) => {
-                // Prevent form from submitting and reloading the page
-                event.preventDefault();
-                // Trim the planet search input and assign it to userSearch
-                const userSearch = planetSearch.value.trim();
+            // Prevent form from submitting and reloading the page
+            event.preventDefault();
+            // Trim the planet search input and assign it to userSearch
+            const userSearch = planetSearch.value.trim();
 
-                // If userSearch has no value, update the error message
-                if (!userSearch) {
-                    errorMsg.textContent = "Sök på en Planet.";
-                    return;
-                }
+            // If userSearch has no value, update the error message
+            if (!userSearch) {
+                errorMsg.textContent = "Sök på en Planet.";
+                return;
+            }
 
-                // Use the filter method to find planets that match the user's search
-                const searchedData = data.bodies.filter(item =>
-                    // Using toLowerCase so that upper or lower case doesn't matter
-                    item.name.toLowerCase() == (userSearch.toLowerCase())
-                );
+            // Use the filter method to find planets that match the user's search
+            const searchedData = data.bodies.filter(item =>
+                // Using toLowerCase so that upper or lower case doesn't matter
+                item.name.toLowerCase() == (userSearch.toLowerCase())
+            );
 
-                // If the search data is not empty, store it in localStorage and navigate to results page
-                if (searchedData.length > 0) {
-                    localStorage.setItem('SearchedData', JSON.stringify(searchedData));
-                    window.location.href = '/JS-EXAM/results.html';
-;
-                } else {
-                    // Update the error message if no planets match the search
-                    errorMsg.textContent = "Kunde inte hitta något resultat.";
-                }
+            // If the search data is not empty, store it in localStorage and navigate to results page
+            if (searchedData.length > 0) {
+                localStorage.setItem('SearchedData', JSON.stringify(searchedData));
+                window.location.href = '/JS-EXAM/results.html';
+            } else {
+                // Update the error message if no planets match the search
+                errorMsg.textContent = "Kunde inte hitta något resultat.";
+            }
             });
         }
     } catch (error) {
@@ -121,75 +120,78 @@ function loadLocalStorage() {
     // Check if the user is on the results page
     if (window.location.pathname.endsWith('results.html')) {
             // If there is stored data, parse the first index of the array to display
-            if (storedData) {
-                const display = JSON.parse(storedData)[0];
-                if (display) {
-                    // Log to check the key and values for displaying
-                    console.log(display);
-                    // changing the html to planetName, latinName and desc.
-                    planetName.textContent = display.name.toUpperCase();
-                    latinName.textContent = display.latinName.toUpperCase();
-                    desc.textContent = display.desc;
-                                    
-                    // creating new element
-                    const newCircumference = document.createElement('p');
-                    // assigning circumference to the new created element, with formatted numbers.
-                    newCircumference.textContent = `${display.circumference.toLocaleString('sv-SE')} km`;
-                    // appending the new child to the parent.
-                    circumference.appendChild(newCircumference);
-                                    
-                    // creating new element
-                    const newDistance = document.createElement('p');
-                    // assigning distance to the new created element, with formatted numbers.
-                    newDistance.textContent = `${display.distance.toLocaleString('sv-SE')} km`;
-                    // appending the new child to the parent.
-                    distance.appendChild(newDistance);
+        if (storedData) {
+            const display = JSON.parse(storedData)[0];
+            if (display) {
+                // Log to check the key and values for displaying
+                console.log(display);
+                // changing the html to planetName, latinName and desc.
+                planetName.textContent = display.name.toUpperCase();
+                latinName.textContent = display.latinName.toUpperCase();
+                desc.textContent = display.desc;
+                                
+                // creating new element
+                const newCircumference = document.createElement('p');
+                // assigning circumference to the new created element, with formatted numbers.
+                newCircumference.textContent = `${display.circumference.toLocaleString('sv-SE')} km`;
+                // appending the new child to the parent.
+                circumference.appendChild(newCircumference);
+                                
+                // creating new element
+                const newDistance = document.createElement('p');
+                // assigning distance to the new created element, with formatted numbers.
+                newDistance.textContent = `${display.distance.toLocaleString('sv-SE')} km`;
+                // appending the new child to the parent.
+                distance.appendChild(newDistance);
 
-                    // creating new element
-                    const newDayTemp = document.createElement('p');
-                    // assigning temp day to the new created element, in Celsius.
-                    newDayTemp.textContent = `${display.temp.day} °C`;
-                    // appending the new child to the parent.
-                    dayTemp.appendChild(newDayTemp);
+                // creating new element
+                const newDayTemp = document.createElement('p');
+                // assigning temp day to the new created element, in Celsius.
+                newDayTemp.textContent = `${display.temp.day} °C`;
+                // appending the new child to the parent.
+                dayTemp.appendChild(newDayTemp);
 
-                    // creating new element
-                    const newNightTemp = document.createElement('p');
-                    // assigning temp night to the new created element, in Celsius.
-                    newNightTemp.textContent = `${display.temp.night} °C`;
-                    // appending the new child to the parent.
-                    nightTemp.appendChild(newNightTemp);
+                // creating new element
+                const newNightTemp = document.createElement('p');
+                // assigning temp night to the new created element, in Celsius.
+                newNightTemp.textContent = `${display.temp.night} °C`;
+                // appending the new child to the parent.
+                nightTemp.appendChild(newNightTemp);
 
-                    // creating new element
-                    const newPlanetType = document.createElement('p');
-                    /* assigning type to the new created element, charAt and toUpperCase
-                    to make the first letter uppercase and then concatenate it with the rest of the word
-                    starting at index 1 */
-                    newPlanetType.textContent = `${display.type.charAt(0).toUpperCase()}${display.type.slice(1)}`;
-                    // appending the new child to the parent.
-                    planetType.appendChild(newPlanetType);
+                // creating new element
+                const newPlanetType = document.createElement('p');
+                /* assigning type to the new created element, charAt and toUpperCase
+                to make the first letter uppercase and then concatenate it with the rest of the word
+                starting at index 1 */
+                newPlanetType.textContent = `${display.type.charAt(0).toUpperCase()}${display.type.slice(1)}`;
+                // appending the new child to the parent.
+                planetType.appendChild(newPlanetType);
 
-                    // Checks if the planet has a moon
-                    if(display.moons.length > 0){
-                         // creating new element
-                        const newMoonUl = document.createElement('ul');
-                        newMoonUl.innerHTML = 'Månar:'
-                        newMoonUl.classList.add('moonInfo');
-                        moonInfo.replaceWith(newMoonUl);
-                       
-                        // forEach incase of its more then 1 moons. 
-                        display.moons.forEach(moon => {
-                            const newMoonLi = document.createElement('li');
-                            newMoonLi.textContent = moon;
-                            // appending the new child to the parent.
-                            newMoonUl.appendChild(newMoonLi);
-                        });
-                    } else {
-                        const newMoonInfo = document.createElement('p');
-                        // if the planet doesn't have any moons, it will display this message.
-                        newMoonInfo.textContent = 'Den här planeten har inga månar.';
-                        // appending the new child to the parent.
-                        moonInfo.appendChild(newMoonInfo);
-                    }
+                // Checks if the planet has a moon
+                if(display.moons.length > 0){
+                    // creating new element
+                    const newMoonUl = document.createElement('ul');
+                    // Set the innerHTML to "Månar" as a header for the list items.
+                    newMoonUl.innerHTML = 'Månar:'
+                    // Adding class list to ul for styling.
+                    newMoonUl.classList.add('moonInfo');
+                    // Replace the paragraph with ul so we can appen list items.
+                    moonInfo.replaceWith(newMoonUl);
+                    
+                    // forEach method, in case of it's more than 1 moons.
+                    display.moons.forEach(moon => {
+                    const newMoonLi = document.createElement('li');
+                    newMoonLi.textContent = moon;
+                    // appending the new child to the parent.
+                    newMoonUl.appendChild(newMoonLi);
+                    });
+                } else {
+                    const newMoonInfo = document.createElement('p');
+                    // if the planet doesn't have any moons, it will display this message.
+                    newMoonInfo.textContent = 'Den här planeten har inga månar.';
+                    // appending the new child to the parent.
+                    moonInfo.appendChild(newMoonInfo);
+                }
 
             }
         } else {
@@ -213,18 +215,18 @@ document.addEventListener('DOMContentLoaded', () => {
         fetchApiKey();
     } else {
         // Otherwise, fetch data using the stored API key
-        fetchDataWithKey(); // Use the stored API key if it exists
+        fetchDataWithKey();
     }
 
     // Check if the back button exists
     if (backButton) {
         // Add an event listener to the back button with a click event
         backButton.addEventListener('click', () => {
-            // When the back button is clicked, navigate to the index page
-            window.location.href = '/JS-EXAM/index.html';
-            // Remove the stored data from localStorage when going back
-            localStorage.removeItem('SearchedData');
-            localStorage.removeItem('apiKey');
+        // When the back button is clicked, navigate to the index page
+        window.location.href = '/JS-EXAM/index.html';
+        // Remove the stored data from localStorage when going back
+        localStorage.removeItem('SearchedData');
+        localStorage.removeItem('apiKey');
         });
     }
 });
